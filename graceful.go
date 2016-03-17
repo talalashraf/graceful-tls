649072985f0fbae12e0ff359ad74ca1fdd245cff
Talal Ashraf <tashraf@surfeasy.com>
2016-03-17 16:44:44 -0400

    Added cipher suites and min TLS12

--- graceful.go
+++ graceful.go
@@ -35,8 +35,34 @@ func ListenAndServeTLS(addr, certFile, keyFile string, handler http.Handler) err
 	if h == nil {
 		h = http.DefaultServeMux
 	}
+	cipherSuites := []uint16{
+    tls.TLS_RSA_WITH_RC4_128_SHA               ,
+    tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA          ,
+    tls.TLS_RSA_WITH_AES_128_CBC_SHA           ,
+    tls.TLS_RSA_WITH_AES_256_CBC_SHA           ,
+    tls.TLS_RSA_WITH_AES_128_GCM_SHA256        ,
+    tls.TLS_RSA_WITH_AES_256_GCM_SHA384        ,
+    tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA       ,
+    tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA   ,
+    tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA   ,
+    tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA         ,
+    tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA    ,
+    tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA     ,
+    tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA     ,
+    tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256  ,
+    tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+    tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384  ,
+    tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+  }
 	if DefaultServer == nil {
-		DefaultServer = NewServer(&http.Server{Addr: addr, Handler: h})
+		DefaultServer = NewServer(&http.Server{
+			Addr: addr,
+			Handler: h,
+			TLSConfig: &tls.Config{
+	      CipherSuites:       cipherSuites,
+	      MinVersion:         tls.VersionTLS12,
+	    },
+			})
 	}
 	return DefaultServer.ListenAndServeTLS(certFile, keyFile)
 }
